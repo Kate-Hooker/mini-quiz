03f8a2efc3fc8384f92beb564b5031293a6d77e1
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-28 11:02:38 +1300

    finished

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ if (answer1.toUpperCase() === correct_answer1) {
 } else {
   console.log('Incorrect!')
 }
+
+const answer2 = prompt('What is the DOM?')
+const correct_answer2 = 'DOCUMENT OBJECT MODEL'
+
+if (answer2.toUpperCase() === correct_answer2) {
+  console.log('Correct!')
+} else {
+  console.log('Incorrect!')
+}
