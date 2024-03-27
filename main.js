04cd7291c412e422801dc5ed01d09e142e7f5238
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-28 10:37:26 +1300

    Prompt sync installed and tested, user input works

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log('hello')
+// prompt sync is an npm package that allows us to prompt the user for input. Terminal command "npm install prompt-sync"
+
+const prompt = require('prompt-sync')()
+
+prompt('Hello, what is your name?')
